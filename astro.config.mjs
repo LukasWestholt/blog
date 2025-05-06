7c076e1e38aa849120751d5f94512881f34742ac
LukasRumi <lukas.ruminski@web.de>
2025-05-06 17:48:30 +0200

    added locales and astro i18next for translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import { siteConfig } from './src/data/site.config'
+import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -29,6 +32,7 @@ export default defineConfig({
 			drafts: true
 		}),
 		sitemap(),
-		tailwind()
+		tailwind(),
+		astroI18next()
 	]
 })
